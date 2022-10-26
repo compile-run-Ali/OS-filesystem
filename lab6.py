5bffac8c35a83dd5289ab8ea9e9d82d4ba9bd50d
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2022-10-26 15:26:02 +0500

    saved to a json

--- lab6.py
+++ lab6.py
@@ -1,78 +1,147 @@
-#implementing an operating system in python
+# implementing an operating system in python
 import json
+
+
 class Folder():
-    def __init__(self, name, location, children = {}):
+    def __init__(self, name, location, children={}):
         self.name = name
         self.location = location
-        self.children=children
+        self.children = children
 
     def get_size(self):
         size = 0
         for child in self.children:
-            size += child.get_size()
+            size += self.children[child].get_size()
         return size
 
     def __str__(self):
-        return self.name + " " + self.location + " " + str(self.size) + " " + str(self.children)
-    
+        return self.name + " " + self.location + " " + str(self.get_size()) + " " + str(self.children)
+
+
 class File():
     def __init__(self, name, location, content):
         self.name = name
         self.location = location
-        self.size = len(content) #assuming 1 character per byte
-        self.content=content
+        self.size = len(content)  # assuming 1 character per byte
+        self.content = content
+
     def get_size(self):
         return self.size
+
     def __str__(self):
         return self.name + " " + self.location + " " + self.size + " " + self.content
-        
 
-#using a dictionary to store pointer to files
+
+# using a dictionary to store pointer to files
 def create_file_system():
     file_system = {}
-    file_system["root"] = Folder("root", "root", [])
+    file_system["root"] = Folder("root", "/", {})
     return file_system
 
- 
-def create_file(file_system, file_name, file_location, content):
-    file_system[file_name] = File(file_name, file_location, content)
-    file_system[file_location].children += [file_system[file_name]]
+
+# take path for folder and then go into the children and store in that directory
+def mkdir(file_system, folder_name, folder_location,originalFolderLocation):
+    path = folder_location.split("/")
+    if (len(path) == 1):
+        print("creating folder in",file_system[folder_location].name)
+        file_system[folder_location].children[folder_name] = Folder(folder_name, originalFolderLocation, {})
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        new_folder_location = "/".join(path)
+        mkdir(file_system[temp].children, folder_name, new_folder_location, originalFolderLocation)
     return file_system
 
-def mkdir(file_system, folder_name, folder_location):
-    file_system[folder_name] = Folder(folder_name, folder_location, [])
-    file_system[folder_location].children += [file_system[folder_name]]
+def delete_file(file_system, file_name, file_location):
+    path = file_location.split("/")
+    if (len(path) == 1):
+        print("deleting file",file_name,"from",file_system[file_location].name)
+        del file_system[file_location].children[file_name]
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        temp_location = "/".join(path)
+        delete_file(file_system[temp].children, file_name, temp_location)
     return file_system
+
+def create_file(file_system, file_name, file_location,originalFolderLocation, content):
+    path = file_location.split("/")
+    if (len(path) == 1):
+        print("creating file in",file_system[file_location].name)
+        file_system[file_location].children[file_name] = File(file_name, originalFolderLocation, content)
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        new_file_location = "/".join(path)
+        create_file(file_system[temp].children, file_name, new_file_location,originalFolderLocation, content)
+    return file_system
+
 
 def ls(file_system, folder_location):
-    for child in file_system[folder_location].children:
-        print(child.name)
+    path = folder_location.split("/")
+    if (len(path) == 1):
+        print("listing folders and files in",file_system[folder_location].name)
+        for child in file_system[folder_location].children:
+            print(child)
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        new_folder_location = "/".join(path)
+        ls(file_system[temp].children, new_folder_location)
     return file_system
 
-def move(file_system, file_name, new_location):
-    file_system[file_name].location.children.remove(file_system[file_name])
-    file_system[file_name].location = new_location
-    file_system[new_location].children += [file_system[file_name]]
+def copy_file(file_system,originalFileSystem, file_name, file_location, new_file_location):
+    path = file_location.split("/")
+    if (len(path) == 1):
+        #moving file to last index of path2
+        print("copying file",file_name,"from",file_system[file_location].name,"to",new_file_location)
+        create_file(originalFileSystem, file_name, new_file_location, new_file_location, file_system[file_location].children[file_name].content)
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        temp_location = "/".join(path)
+        copy_file(file_system[temp].children,originalFileSystem, file_name, temp_location, new_file_location)
     return file_system
-#open file should return file object and all read write move and truncate done through this
 
-def open_file(file_system, file_name):
-    return file_system[file_name]
+def move_file(file_system,original_file_system, file_name, file_location, new_file_location):
+    path = file_location.split("/")
+    if (len(path) == 1):
+        #moving file to last index of path2
+        print("moving file",file_name,"from",file_system[file_location].name,"to",new_file_location)
+        print( file_system[file_location].children[file_name].content)
+        create_file(original_file_system, file_name, new_file_location, new_file_location, file_system[file_location].children[file_name].content)
+        delete_file(original_file_system, file_name, file_location)
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        temp_location = "/".join(path)
+        move_file(file_system[temp].children,original_file_system, file_name, temp_location, new_file_location)
+    return file_system
+# open file should return file object and all read write move and truncate done through this
 
-def read(file_object, num_bytes):
-    return file_object.content[:num_bytes]
+def open_file(file_system, file_name, file_location):
+    path = file_location.split("/")
+    if (len(path) == 1):
+        return file_system[file_location].children[file_name]
+    else:
+        # remove index 0 from path
+        temp=path.pop(0)
+        temp_location = "/".join(path)
+        return open_file(file_system[temp].children, file_name, temp_location)
+    
+#read whole file
+def read(file_object):
+    return file_object.content
 
-def write(file_object, content):
-    file_object.content += content
-    file_object.size += len(content)
+def write(file_object, start, end, content):
+    file_object.content = file_object.content[:start] + content + file_object.content[end:]
     return file_object
 
-def truncate(file_object, num_bytes):
-    file_object.content = file_object.content[:num_bytes]
-    file_object.size = num_bytes
+def truncate(file_object, size):
+    file_object.content = file_object.content[:size]
     return file_object
 
-def close(file_object):
+def close_file(file_object):
     return file_object
 
 def moveContentWithinFile(file_object, start, end, new_start):
@@ -81,19 +150,84 @@ def moveContentWithinFile(file_object, start, end, new_start):
     file_object.content = file_object.content[:new_start] + content + file_object.content[new_start:]
     return file_object
 
-def showMemoryMap(file_system):
-    for key in file_system:
-        print(key, file_system[key].get_size())
+# I wanna show memory map as a tree
+class customEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Folder):
+            return obj.__dict__
+        if isinstance(obj, File):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
+
+def show_memory_map(file_system):
+    with open('memory_map.json', 'w') as outfile:
+        json.dump(file_system, outfile, cls=customEncoder, indent=4)
     return file_system
 
-file_system=create_file_system()
-mkdir(file_system, "folder1", "root")
-mkdir(file_system, "folder2", "folder1")
-create_file(file_system, "file1", "folder1", "hello")
-create_file(file_system, "file2", "folder2", "hello")
-create_file(file_system, "file3", "folder2", "hello")
-mkdir(file_system, "folder3", "folder2")
-create_file(file_system, "file4", "folder3", "hello")
-create_file(file_system, "file5", "folder3", "hello")
+#recursivelt printing and size
+def print_file_system(file_system):
+    for child in file_system:
+        if isinstance(file_system[child], Folder):
+            print_file_system(file_system[child].children)
+        else:
+            #folder size
+            print(file_system[child].name, file_system[child].location, file_system[child].content)
+            print(file_system[child].get_size())
+    return file_system
+def print_folders(file_system):
+    for child in file_system:
+        if isinstance(file_system[child], Folder):
+            print(file_system[child].name, file_system[child].location,"Size is", file_system[child].get_size())
+            print_folders(file_system[child].children)
+    return file_system
 
-showMemoryMap(file_system)
+
+
+
+file_system=create_file_system()
+file_system=mkdir(file_system,"folder1","root","root/folder1")
+ 
+file_system=mkdir(file_system,"folder2","root","root/folder2")
+file_system=mkdir(file_system,"folder3","root","root/folder3")
+
+file_system=mkdir(file_system,"folder4","root/folder1","root/folder1/folder4")
+
+file_system=mkdir(file_system,"folder5","root/folder1/folder4","root/folder1/folder4/folder5")
+
+#files
+file_system=create_file(file_system,"file1","root","root","file1")
+file_system=create_file(file_system,"file2","root","root","file2")
+ 
+file_system=create_file(file_system,"file3","root/folder1","root/folder1","file3")
+
+#using ls
+ls(file_system, "root")
+ls(file_system, "root/folder1")
+
+#moving file 2
+file_system=move_file(file_system,file_system,"file1","root","root/folder1")
+
+#show 
+show_memory_map(file_system)
+
+#size of each element
+print_folders(file_system)
+
+#open
+file_object=open_file(file_system,"file1","root/folder1")
+
+#read
+print(read(file_object))
+
+#write
+
+#truncate
+
+
+#move content
+moveContentWithinFile(file_object,0,2,2)
+
+print(read(file_object))
+#close
+close_file(file_object)
+
